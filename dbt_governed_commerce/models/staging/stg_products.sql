948395b2ae833f361482b6047ce08c63e108c63c
SAURABH SINGH <draganovsmac@SAURABHs-MacBook-Air.local>
2026-07-17 00:06:40 +0530

    mart models

--- dbt_governed_commerce/models/staging/stg_products.sql
+++ dbt_governed_commerce/models/staging/stg_products.sql
@@ -1,13 +1,19 @@
-select 
-product_id
-,product_category_name
-,product_name_lenght product_name_length
-,product_description_lenght product_description_length
-,product_photos_qty product_photos_qty
-,product_weight_g
-,product_length_cm
-,product_height_cm
-,product_width_cm
-,ingestion_file_name
-,ingestion_timestamp
-from {{ source('raw', 'products_raw') }}
+select
+    p.product_id,
+    p.product_category_name,
+    coalesce(
+        t.product_category_name_english,
+        p.product_category_name
+    ) as product_category_name_english,
+    p.product_name_lenght as product_name_length,
+    p.product_description_lenght as product_description_length,
+    p.product_photos_qty,
+    p.product_weight_g,
+    p.product_length_cm,
+    p.product_height_cm,
+    p.product_width_cm,
+    p.ingestion_file_name,
+    p.ingestion_timestamp
+from {{ source('raw', 'products_raw') }} as p
+left join {{ source('raw', 'category_translation_raw') }} as t
+    on p.product_category_name = t.product_category_name
--- dbt_governed_commerce/models/staging/stg_sellers.sql
+++ dbt_governed_commerce/models/staging/stg_sellers.sql
@@ -1,4 +1,8 @@
-select 
-seller_id
-,
+select
+    seller_id,
+    seller_zip_code_prefix,
+    lower(seller_city) as seller_city,
+    upper(seller_state) as seller_state,
+    ingestion_file_name,
+    ingestion_timestamp
 from {{ source('raw', 'sellers_raw') }}
